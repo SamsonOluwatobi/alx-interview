db69cc996409451e23a6d2ce4fac7643b9fe3b01
Samson <samtob2002@gmail.com>
2024-12-03 12:31:31 +0100

    chore: adding module doc

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,6 +1,37 @@
 #!/usr/bin/python3
+"""
+Coin Change Module
+
+This module provides a function `makeChange` to determine the minimum number
+of coins required to make up a given total amount, using the provided
+coin denominations.
+
+The function supports:
+- Greedy algorithm for quick solutions.
+- Dynamic programming for correctness with arbitrary denominations.
+
+Typical Use Cases:
+- Calculating change for vending machines.
+- Solving optimization problems related to currency denominations.
+- Algorithmic challenges involving greedy or dynamic programming approaches.
+
+"""
+
+
 def makeChange(coins, total):
-    """Determines the fewest number of coins needed to make up a given total."""
+    """
+    Determines the fewest number of coins needed to meet a given total amount.
+
+    Args:
+        coins (list): A list of integers representing the denominations
+        of coins available.
+        total (int): The total amount for which change is required.
+
+    Returns:
+        int: The minimum number of coins needed to make up the total.
+             Returns 0 if total is 0 or less.
+             Returns -1 if the total cannot be met with the given coins.
+    """
     if total <= 0:
         return 0
 
